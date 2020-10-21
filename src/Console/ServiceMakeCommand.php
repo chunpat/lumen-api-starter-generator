3d45f60b4756a1283f35d0c05bac77791269b3bb
chunpat <chunpat@163.com>
2020-10-21 17:26:32 +0800

    init

---
+++ src/Console/ServiceMakeCommand.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Chunpat\LumenApiStarterGenerator\Console;
+
+use Illuminate\Support\Str;
+use Symfony\Component\Console\Input\InputOption;
+
+class ServiceMakeCommand extends GeneratorCommand
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'make:service';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new service';
+
+    /**
+     * The type of class being generated.
+     *
+     * @var string
+     */
+    protected $type = 'Service';
+
+    /**
+     * Get the stub file for the generator.
+     *
+     * @return string
+     */
+    protected function getStub()
+    {
+        return __DIR__.'/stubs/service.full.stub';
+//        return $this->collection()
+//                    ? __DIR__.'/stubs/resource-collection.stub'
+//                    : __DIR__.'/stubs/service.stub';
+    }
+
+    /**
+     * Determine if the command is generating a resource collection.
+     *
+     * @return bool
+     */
+    protected function collection()
+    {
+        return $this->option('collection') ||
+               Str::endsWith($this->argument('name'), 'Collection');
+    }
+
+    /**
+     * Get the default namespace for the class.
+     *
+     * @param  string  $rootNamespace
+     * @return string
+     */
+    protected function getDefaultNamespace($rootNamespace)
+    {
+        return $rootNamespace.'\Services';
+    }
+
+    /**
+     * Get the console command options.
+     *
+     * @return array
+     */
+    protected function getOptions()
+    {
+        return [
+            ['collection', 'c', InputOption::VALUE_NONE, 'Create a resource collection.'],
+        ];
+    }
+}
